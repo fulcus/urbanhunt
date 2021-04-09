8906eba0428edf274f749794008ca208dac6c0ef
fulcus <francescofgonzales@gmail.com>
2021-04-09 02:03:39 +0200

    created firebase function to add user

---
+++ firebase/functions/index.js
@@ -0,0 +1,9 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin"); 
+
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.addUser = functions.auth.user().onCreate((user) => {
+  db.collection("users").doc(user.uid).set({ score: 0 });
+});
