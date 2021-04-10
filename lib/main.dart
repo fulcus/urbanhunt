aa9a72f59e37e71a8c77ad2ebb87d246bf564d42
fulcus <francescofgonzales@gmail.com>
2021-04-10 19:16:08 +0200

    added counter as example of write to db

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import './auth.dart';
+import './counter.dart';
 import './home.dart';
 
 Future<void> main() async {
@@ -15,10 +16,11 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hunt Your Picture',
+      home: CounterHome(title: 'Counter'),
       //home: const HomePage(title: 'Hunt App'),
-      home: Scaffold(
+      /*home: Scaffold(
         body: AuthTypeSelector(),
-      ),
+      ),*/
       theme: ThemeData(
         primarySwatch: Colors.green,
         scaffoldBackgroundColor: Colors.greenAccent[50],
