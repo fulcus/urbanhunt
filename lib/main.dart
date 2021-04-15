b4b9c39622a4f5eebbce27aaae611ec91b3b937f
mrossetti <rossetti.marcolm@gmail.com>
2021-04-15 17:46:25 +0200

    edited main to test a sample place page

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:hunt_app/place_page.dart';
 import './auth.dart';
 import './home.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,8 +19,19 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Hunt Your Picture',
       //home: const HomePage(title: 'Hunt App'),
-      home: Scaffold(
-        body: AuthTypeSelector(),
+      home: PlacePage(
+        placeData: PlaceData(
+          id: 1,
+          name: "Posto mistico 1",
+          categories: ["mistico1", "mistico2", "mistico3"],
+          descriptionUnlocked: "Unlocked - Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem.",
+          descriptionLocked: "Locked - Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem.",
+          imagePathUnlocked: "res/A.png",
+          imagePathLocked: "res/B.png",
+          address: "Viale dei Viali 2, Mi",
+          latitude: 3.14159,
+          longitude: 6.28318,
+        ),
       ),
       theme: ThemeData(
         primarySwatch: Colors.green,
