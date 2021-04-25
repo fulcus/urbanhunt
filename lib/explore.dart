4dca3492e02cc6c6c65dc06be5d779b0085b67f8
LorenzoMainetti <61703143+LorenzoMainetti@users.noreply.github.com>
2021-04-25 17:54:46 +0200

    Added custom myLocationButton and compassButton and modified query to retrieve unlockedPlaces of the user

--- lib/explore.dart
+++ lib/explore.dart
@@ -1,20 +1,17 @@
-
 import 'dart:async';
-import 'dart:ui';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:url_launcher/url_launcher.dart';
-
-import 'counter.dart';
+import 'package:hunt_app/home.dart';
 
 
 final db = FirebaseFirestore.instance;
+final userId = FirebaseAuth.instance.currentUser!.uid;
 bool loading = true;
-
 
 class Home extends StatefulWidget {
   @override
@@ -23,6 +20,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   late Stream<QuerySnapshot> _places;
+  late Stream<QuerySnapshot> _unlockedPlaces;
   final Completer<GoogleMapController> _mapController = Completer();
 
   double _initLat = 0.0;
@@ -33,11 +31,20 @@ class _HomeState extends State<Home> {
   void initState() {
     super.initState();
 
-    _places = FirebaseFirestore.instance
+    //retrieve all the places
+    _places = db
         .collection('places')
         .orderBy('name')
         .snapshots();
 
+    //retrieve the user's unlocked places
+    _unlockedPlaces = db
+        .collection('users')
+        .doc(userId)
+        .collection('unlockedPlaces')
+        .snapshots();
+
+    //retrieve the user's initial position
     _determinePosition().then((value) {
       setState(() {
         _initLat = value.latitude;
@@ -46,7 +53,6 @@ class _HomeState extends State<Home> {
       });
     });
   }
-
 
   /// Determine the current position of the device.
   ///
@@ -80,8 +86,7 @@ class _HomeState extends State<Home> {
         // Android's shouldShowRequestPermissionRationale
         // returned true. According to Android guidelines
         // your App should show an explanatory UI now.
-        return Future.error(
-            'Location permissions are denied');
+        return Future.error('Location permissions are denied');
       }
     }
 
@@ -102,41 +107,40 @@ class _HomeState extends State<Home> {
       body: StreamBuilder<QuerySnapshot>(
         stream: _places,
         builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          }
-          if (!snapshot.hasData) {
-            return Center(child: const Text('Loading...'));
-          }
-
-          return Stack(
-            children: [
-              StoreMap(
-                documents: snapshot.data!.docs,
-                initialPosition: LatLng(_initLat, _initLng),
-                mapController: _mapController,
-              ),
-            ],
+          return StreamBuilder<QuerySnapshot>(
+              stream: _unlockedPlaces,
+              builder: (context, snapshot2) {
+                if (snapshot.hasError || snapshot2.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                }
+                if (!snapshot.hasData || !snapshot2.hasData) {
+                  return Center(child: CircularProgressIndicator(),);
+                }
+                return
+                  StoreMap(
+                    places: snapshot.data!.docs,
+                    unlockedPlaces: snapshot2.data!.docs,
+                    initialPosition: LatLng(_initLat, _initLng),
+                    mapController: _mapController,
+                  );
+              }
           );
         },
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-              icon: Icon(Icons.explore),
-             label: 'Map',
+            icon: Icon(Icons.explore),
+            label: 'Map',
           ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.add),
-              label: 'Contribute',
+            icon: Icon(Icons.add),
+            label: 'Contribute',
           ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Social'),
           BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Social'
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: 'Settings',
+            icon: Icon(Icons.settings),
+            label: 'Settings',
           ),
         ],
         currentIndex: _selectedIndex,
@@ -148,16 +152,16 @@ class _HomeState extends State<Home> {
 }
 
 class StoreMap extends StatefulWidget {
-
   StoreMap({
     Key? key,
-    required this.documents,
+    required this.places,
+    required this.unlockedPlaces,
     required this.initialPosition,
     required this.mapController,
   }) : super(key: key);
 
-  final String userId = 'Kd5combpKoh1gLYyYUyftiAwcbP2'; //get from auth (look for the right method to get id)
-  final List<DocumentSnapshot> documents;
+  final List<DocumentSnapshot> places;
+  final List<DocumentSnapshot> unlockedPlaces;
   final LatLng initialPosition;
   final Completer<GoogleMapController> mapController;
 
@@ -166,8 +170,12 @@ class StoreMap extends StatefulWidget {
 }
 
 class _StoreMapState extends State<StoreMap> {
-
   Set<Marker> customMarkers = {};
+  double _currentCameraBearing = 0.0;
+  double _currentCameraTilt = 0.0;
+  double _currentLat = initialPosition.latitude;
+  double _currentLng = initialPosition.longitude;
+  double _currentZoom = 12.0;
 
   @override
   void initState() {
@@ -177,61 +185,122 @@ class _StoreMapState extends State<StoreMap> {
 
   @override
   Widget build(BuildContext context) {
-    if(loading == false) {
-      return GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: widget.initialPosition,
-          zoom: 12,
-        ),
-        markers: customMarkers,
-        onMapCreated: (mapController) {
-          widget.mapController.complete(mapController);
-        },
-        padding: EdgeInsets.only(top: 680.0, left: 310.0), //+ MediaQuery.of(context).padding -> safe area
-        myLocationEnabled: true,
-        myLocationButtonEnabled: true,
-        compassEnabled: true,
+    if (loading == false) {
+      return Stack(children: [
+        GoogleMap(
+          initialCameraPosition: CameraPosition(
+            target: widget.initialPosition,
+            zoom: 12,
+          ),
+          markers: customMarkers,
+          onMapCreated: (mapController) {
+            widget.mapController.complete(mapController);
+            setState(() {});
+          },
+          onCameraMove: _updateCameraInfo,
+          myLocationEnabled: true,
 
-        mapToolbarEnabled: false,
-        zoomControlsEnabled: false,
-        //indoorViewEnabled: true, => we might need it
-      );
-    }
-    else {
+          compassEnabled: false,
+          myLocationButtonEnabled: false,
+          mapToolbarEnabled: false,
+          zoomControlsEnabled: false,
+          //indoorViewEnabled: true, => we might need it
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 680.0, left: 350.0),
+          child: CircleAvatar(
+            backgroundColor: Colors.amber,
+            child: IconButton(
+              icon: Icon(Icons.my_location),
+              color: Colors.black,
+              onPressed: _setCurrentLocation,
+            ),
+          ),
+        ),
+        if(_currentCameraBearing != 0.0)
+          Padding(
+            padding: EdgeInsets.only(top: 630.0, left: 350.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.amber,
+              child: IconButton(
+                icon: Icon(Icons.explore),
+                color: Colors.black,
+                onPressed: _rotateNorth,
+              ),
+            ),
+          )
+      ]);
+    } else {
       return Center(
         child: CircularProgressIndicator(),
       );
     }
   }
 
+  void _updateCameraInfo(CameraPosition cameraPosition) {
+    _currentCameraBearing = cameraPosition.bearing;
+    _currentCameraTilt = cameraPosition.tilt;
+    _currentLat = cameraPosition.target.latitude;
+    _currentLng = cameraPosition.target.longitude;
+    _currentZoom = cameraPosition.zoom;
+    setState(() {});
+  }
+
+  Future<void> _setCurrentLocation() async {
+    var currentLocation = await Geolocator.getCurrentPosition();
+    var cPosition = CameraPosition(
+      zoom: 12,
+      bearing: _currentCameraBearing,
+      tilt: _currentCameraTilt,
+      target: LatLng(currentLocation.latitude, currentLocation.longitude),
+    );
+    final controller = await widget.mapController.future;
+    await controller
+        .animateCamera(CameraUpdate.newCameraPosition(cPosition))
+        .then((value) {
+      setState(() {});
+    });
+  }
+
+  Future<void> _rotateNorth() async {
+    var cPosition = CameraPosition(
+      zoom: _currentZoom,
+      bearing: 0.0,
+      target: LatLng(_currentLat, _currentLng),
+    );
+    final controller = await widget.mapController.future;
+    await controller
+        .animateCamera(CameraUpdate.newCameraPosition(cPosition))
+        .then((value) {
+      setState(() {});
+    });
+  }
+
   Future<void> _setCustomMarkers() async {
     var markers = <Marker>{};
-    var check = true;
 
     late BitmapDescriptor unlockedImg;
     late BitmapDescriptor lockedImg;
 
+    unlockedImg =
+        await _createMarkerImageFromAsset('assets/images/open-lock.png');
+    lockedImg =
+        await _createMarkerImageFromAsset('assets/images/locked-padlock.png');
 
-    unlockedImg = await _createMarkerImageFromAsset('assets/images/open-lock.png');
-    lockedImg = await _createMarkerImageFromAsset('assets/images/locked-padlock.png');
-
-    for (var document in widget.documents) {
-
-      check = await _checkDocInCollection(document);
-      if (check) {
+    for (var document in widget.places) {
+      if(widget.unlockedPlaces.where((element) => element.id == document.id).isEmpty) {
         markers.add(Marker(
           markerId: MarkerId(document.id),
-          icon: unlockedImg,
+          icon: lockedImg,
           position: LatLng(
             document['location'].latitude as double,
             document['location'].longitude as double,
           ),
         ));
-      }
-      else {
+      } else {
         markers.add(Marker(
           markerId: MarkerId(document.id),
-          icon: lockedImg,
+          icon: unlockedImg,
           position: LatLng(
             document['location'].latitude as double,
             document['location'].longitude as double,
@@ -242,36 +311,8 @@ class _StoreMapState extends State<StoreMap> {
     customMarkers = markers;
   }
 
-  Future <BitmapDescriptor> _createMarkerImageFromAsset(String iconPath) async {
+  Future<BitmapDescriptor> _createMarkerImageFromAsset(String iconPath) async {
     return await BitmapDescriptor.fromAssetImage(ImageConfiguration(), iconPath);
   }
 
-  Future<bool> _checkDocInCollection(DocumentSnapshot doc) async {
-    final snapShot = await db.collection('users')
-                             .doc(widget.userId)
-                             .collection('unlockedPlaces')
-                             .doc(doc.id)
-                             .get();
-    if (!snapShot.exists) {
-      return false;
-    }
-    else {
-      return true;
-    }
-  }
-
-  //Function to be called when the user wants to open the selected place in Google Maps.
-  //Arguments -> latitude and longitude of the place
-  static Future<void> openMap(double latitude, double longitude) async {
-    var googleUrl = 'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude';
-    if (await canLaunch(googleUrl)) {
-      await launch(googleUrl);
-    } else {
-      throw 'Could not open the map.';
-    }
-  }
-
 }
-
-
-
