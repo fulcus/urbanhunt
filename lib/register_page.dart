e19789ea964f9abf6749661627157ead41dc7540
fulcus <francescofgonzales@gmail.com>
2021-08-16 18:52:15 +0200

    added username to registration

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -1,8 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_signin_button/button_builder.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
+final db = FirebaseFirestore.instance;
+final FirebaseStorage storage = FirebaseStorage.instance;
 
 /// Entrypoint example for registering via Email/Password.
 class RegisterPage extends StatefulWidget {
@@ -15,6 +19,7 @@ class RegisterPage extends StatefulWidget {
 
 class _RegisterPageState extends State<RegisterPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
@@ -35,6 +40,16 @@ class _RegisterPageState extends State<RegisterPage> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
+                  TextFormField(
+                    controller: _usernameController,
+                    decoration: const InputDecoration(labelText: 'Username'),
+                    validator: (String? value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),
                   TextFormField(
                     controller: _emailController,
                     decoration: const InputDecoration(labelText: 'Email'),
@@ -90,6 +105,7 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   void dispose() {
     // Clean up the controller when the Widget is disposed
+    _usernameController.dispose();
     _emailController.dispose();
     _passwordController.dispose();
     super.dispose();
@@ -97,18 +113,30 @@ class _RegisterPageState extends State<RegisterPage> {
 
   // Example code for registration.
   Future<void> _register() async {
-    final User? user = (await _auth.createUserWithEmailAndPassword(
+    final user = (await _auth.createUserWithEmailAndPassword(
       email: _emailController.text,
       password: _passwordController.text,
     ))
         .user;
-    if (user != null) {
+
+    //save username to firestore User object and db field
+    // todo check username uniqueness
+    await user
+        ?.updateDisplayName(_usernameController.text)
+        .then((value) => db
+            .collection('users')
+            .doc(user.uid)
+            .update({'username': _usernameController.text}))
+        .then((_) {
       setState(() {
         _success = true;
         _userEmail = user.email!;
       });
-    } else {
+    }).catchError((Error error) {
       _success = false;
-    }
+      print(error);
+    });
+    //upload pic to storage
+    //await user?.updatePhotoURL(user.uid); // photoURL is uid
   }
 }
